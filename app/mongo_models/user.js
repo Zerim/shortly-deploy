085bb2bf783731ed5c63d2b1110f0b93511df1be
Hack Reactor Students <accounts@hackreactor.com>
2016-03-31 22:00:25 -0700

    Implement user registration at the /signup path leveraging mongoose models.

--- app/mongo_models/user.js
+++ app/mongo_models/user.js
@@ -1,4 +1,7 @@
 var db = require('./config.js');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
 
 // var userId = db.Schema.Types.ObjectId();
 // userId.auto(true);
@@ -10,6 +13,26 @@ var UserSchema = db.Schema({
   createdAt: {type: Date, Default: Date.now}
 });
 
+UserSchema.methods.comparePassword = function(attempted, callback) {
+  console.log('COMPARING TO   ', this.get('password'));
+  bcrypt.compare(attempted, this.get('password'), function(err, isMatch) {
+    callback(isMatch);
+  });
+};
+
+UserSchema.methods.hashPassword = function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+  cipher(this.get('password'), null, null).bind(this)
+    .then(function(hash) {
+      console.log(hash, '******************  THIS:    ', this);
+      this.set({'password': hash});
+    }.bind(this));
+};
+
+UserSchema.pre('save', function(next) {
+  this.hashPassword();
+  next();
+});
 
 
 module.exports = db.model('User', UserSchema);
